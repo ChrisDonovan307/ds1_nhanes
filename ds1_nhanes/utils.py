c2bd969c42f26eaf33d76f017e1d557ac556f589
Christopher.Donovan <christopher.donovan@uvm.edu>
2025-04-14 20:44:34 -0400

    reworking missing data and imputation, adding spectral clustering, more utils functions

--- ds1_nhanes/utils.py
+++ ds1_nhanes/utils.py
@@ -26,3 +26,39 @@ def graph_plotly(df, labels, title=None, *args, **kwargs):
         title=title
     )
     return fig
+
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import NearestNeighbors
+from sklearn.metrics import adjusted_rand_score
+from sklearn.cluster import KMeans
+
+def split_half_val(df, k=3, random_state=None):
+    # Split data in half
+    train, test = train_test_split(df, test_size=0.5, random_state=random_state)
+
+    # Cluster training half
+    km_train = KMeans(n_clusters=k, random_state=random_state).fit(train)
+    train_labels = km_train.labels_
+
+    # NN to pair each training point with a test point
+    nn = NearestNeighbors(n_neighbors=1).fit(train)
+    distances, indices = nn.kneighbors(test)
+    nn_labels = train_labels[indices.flatten()]
+
+    # Cluster test set
+    km_test = KMeans(n_clusters=k, random_state=random_state).fit(test)
+    test_labels = km_test.labels_
+
+    # Get Adjusted Rand Scores that compare test labels to nn labels
+    ari = adjusted_rand_score(test_labels, nn_labels)
+    return ari
+
+# Second function just makes iterations easier
+def iterate_val(df, k=3, n_iter=10, random_state=42):
+    aris = []
+    for i in range(n_iter):
+        ari = split_half_val(df, k=k, random_state=random_state + i)
+        aris.append(ari)
+    return aris
---
+++ notebooks/9_missing_data.R
@@ -0,0 +1,234 @@
+# Housekeeping -------------------------
+
+if (!require('pacman')) install.packages('pacman')
+pacman::p_load(
+  dplyr,
+  purrr,
+  naniar,
+  ggplot2,
+  skimr,
+  missForest,
+  ggpubr,
+  stringr,
+  lmtest,
+  forcats,
+  pscl,
+  tictoc
+)
+pacman::p_load_gh('ChrisDonovan307/projecter')
+full <- read.csv('data/clean/nhanes_2017_2023_clean.csv')
+get_str(full)
+
+# For now we don't care about missing in prop_pbp
+# That is a separate issue. Let's remove it
+# Also ditch income quartiles, same missingness as og income
+full <- select(full, -any_of(c(
+  'prop_pbp',
+  'oz_pbp',
+  'pf_total_calc',
+  'SEQN',
+  'weight_2d',
+  'grams'
+)))
+
+# Turn demos into factors
+full <- full %>%
+    mutate(
+        race = factor(race),
+        education = factor(education),
+        education = fct_relevel(
+            education,
+            'Less than 9th grade',
+            '9th to 11th grade',
+            'High school/GED',
+            'Some college or AA',
+            'College graduate or above'
+        ),
+        gender = factor(gender),
+        income_ratio_qs = factor(income_ratio_qs),
+        income_ratio_qs = fct_relevel(
+            income_ratio_qs,
+            'Lowest',
+            'Low',
+            'Medium',
+            'High',
+            'Highest'
+        )
+    )
+get_str(full)
+
+
+
+# Visualize ---------------------------------------------------------------
+
+
+# Get total missing from full dataset
+mean(is.na(select(full, -income_ratio_qs))) * 100
+# 4.2496% missing overall
+
+# Not missing anything from fped variables
+# Let's narrow down to just demos and biomarkers
+dat <- select(full, gender:last_col())
+get_str(dat)
+
+vis_miss <- vis_miss(
+    select(dat, -income_ratio_qs),
+    warn_large_data = FALSE,
+    cluster = TRUE,
+    show_perc = TRUE
+)
+vis_miss +
+  theme(
+    plot.margin = margin(0, 50, 0, 0),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()
+  )
+# Some income missing, and lots of biomarkers.
+
+# Let's save it
+ggsave(
+  'outputs/checkin_3/vis_miss.png',
+  width = 6,
+  height = 6,
+  units = 'in',
+  bg = 'white'
+)
+
+# Check by variable
+miss_var_summary(dat)
+gg_miss_var(dat, show_pct = TRUE)
+# serum ferritin is 43%. All biomarkers missing > 10%
+
+# Save it
+ggsave(
+  'outputs/checkin_3/gg_miss_var.png',
+  width = 4,
+  height = 5,
+  units = 'in',
+  bg = 'white'
+)
+
+# Explore missing data by demographics
+no_qs <- select(dat, -income_ratio_qs)
+demos <- c('race', 'education', 'age', 'gender')
+gg_miss_fcts <- list()
+
+# Map over them to plot at once
+plots <- map(demos, ~ {
+  gg_miss_fct(no_qs, !!sym(.x))
+}) %>%
+  setNames(c(demos))
+
+# Add hinky one
+plots$income <- gg_miss_fct(select(dat, -income_ratio), income_ratio_qs)
+plots
+
+# Save all of them
+iwalk(plots, ~ {
+  ggsave(
+    filename = paste0('outputs/checkin_3/gg_miss_fct_', .y, '.png'),
+    plot = .x,
+    width = 5,
+    height = 6,
+    units = 'in',
+    bg = 'white'
+  )
+})
+
+
+
+# Nature of Missingness -------------------------
+
+
+# Test for MCAR
+mcar_test(dat)
+# Null hypothesis is MCAR. Significant result suggests not MCAR
+
+# For each variable with any missing, make a var that is its missingness as a
+# binary, then run logistic regression to test for MAR
+indices <- map_lgl(names(dat), ~ {
+  any(is.na(dat[[.x]]))
+})
+miss_vars <- names(dat)[indices]
+
+# Make all models
+models <- map(miss_vars, ~ {
+  df <- dat %>%
+    mutate(missing = ifelse(is.na(!!sym(.x)), 1, 0))
+  if (.x == 'income_ratio') {
+    model <- glm(
+      missing ~ age + gender + race + education,
+      data = df,
+      family = 'binomial'
+    )
+  } else {
+    model <- glm(
+      missing ~ age + gender + race + education + income_ratio,
+      data = df,
+      family = 'binomial'
+    )
+  }
+  return(model)
+}) %>%
+  setNames(c(miss_vars))
+get_str(models)
+
+# Check summaries
+map(models, summary)
+# Every model has significant predictors
+
+# Get psuedo R2 for each model
+pR2s <- map_dbl(models, ~ pR2(.x)[['McFadden']])
+pR2s
+range(pR2s)
+# pseudo R2 ranges from 0.01575 to 0.4876
+
+
+
+# Imputation --------------------------------------------------------------
+
+
+# Using missForest to impute data and create clean dataset
+# Can use FPED variables to help predict
+get_str(full)
+get_str(dat)
+
+input <- dat %>%
+  select(-any_of(c(
+    'income_ratio_qs',
+    'serum_ferritin',
+    'glycohemoglobin'
+  )))
+get_str(input)
+
+# Run missForest
+set.seed(42)
+get_time()
+tic()
+mf_out <- missForest(
+  input,
+  ntree = 100,
+  mtry = 4,
+  variablewise = TRUE,
+  verbose = TRUE
+)
+toc()
+# This is ridiculously slow
+
+get_str(mf_out)
+
+# Check out imputation error
+mf_out$OOBerror
+# MSE is 2.044703
+
+# Pull out clean dataset
+clean <- mf_out$ximp
+get_str(clean)
+
+
+
+# Save --------------------------------------------------------------------
+
+
+# Save as csv for EDA script
+# write.csv(clean, 'data/clean/nhanes_2017_2023_imputed.csv')
